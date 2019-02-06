12488a1f29f9035b3af49dae45602503476cf4ca
Pål Grønstad Solheim <paul.solheim@gmail.com>
2019-02-06 16:55:13 +0100

    hit 6.
    så langt vi kom.

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -2,4 +2,11 @@ extends CanvasLayer
 
 
 func _on_Button_pressed():
-	pass
+	get_parent().restart_game()
+
+func update_score(player, score):
+	var score_label = get_node("Banner/HBoxContainer/Player%sScore" % player)
+	score_label.text = str(score)
+
+func game_over():
+	$Popup.popup_centered()
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,5 +1,9 @@
 extends Spatial
 
+var Player1_score = 0
+var Player2_score = 0
+export var maximum_score = 2
+
 onready var Ball = get_tree().get_root().find_node("Ball", true, false)
 
 func _ready():
@@ -19,8 +23,27 @@ func _on_GoalDetection_body_entered(body, player_id):
 	Ball.axis_lock_linear_z = true
 	print("Player " + str(player_id) + " has scored!")
 	get_tree().call_group("player", "can_move", false)
+	update_score(player_id)
 	$Timer.start()
 
+func update_score(player):
+	var new_score
+	if player == 1:
+		Player1_score += 1
+		new_score = Player1_score
+	else:
+		Player2_score += 1
+		new_score = Player2_score
+	$GUI.update_score(player, new_score)
+	check_game_over(new_score)
+	
+func check_game_over(score):
+	if score == maximum_score:
+		$Timer.queue_free()
+		$GUI.game_over()
+
+func restart_game():
+	get_tree().reload_current_scene()
 
 func _on_Timer_timeout():
 	reset_pitch()
